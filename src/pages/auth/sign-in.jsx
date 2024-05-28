47eef7a5cd1921d9201082e71c499252e260f118
noumbissi16 <noumbissistael@gmail.com>
2024-05-28 22:10:21 +0100

    Styled the auth routes with tailwind css

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaFacebook, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const SingIn = () => {
@@ -9,7 +10,7 @@ const SingIn = () => {
       // const res = await fetch("http://localhost:8080/auth/google/callback");
       // const data = await res.json();
       // console.log(data);
-      window.open("http://localhost:8080/auth/google/callback", "_self");
+      // window.open("http://localhost:8080/auth/google/callback", "_self");
     } catch (error) {
       console.log("SingIn error", error);
     } finally {
@@ -17,59 +18,72 @@ const SingIn = () => {
     }
   }
   return (
-    <div className="flex justify-center flex-col w-6/12 m-auto">
+    <div className="flex justify-center items-center w-full h-[100vh]">
       {/* <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2> */}
-      <h2 className="text-center mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-700">
-        Sign in to your account
-      </h2>
-
-      <form className="border-solid space-y-6">
-        <div className=" mt-2">
+      <form className="w-[90vw] m-auto shadow-md hover:shadow-lg p-5 rounded-lg md:w-[600px]">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-700 mb-4">
+          Sign In to your account
+        </h2>
+        <div className="my-4">
           <label
             htmlFor="email"
-            className="block text-sm font-medium leading-6 text-gray-800"
+            className="block text-sm font-medium leading-6 text-gray-800 mb-1"
           >
             Email address
           </label>
-          <div className="mt-1">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              autoComplete="email"
-              required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-indigo-700 sm:text-sm sm:leading-6"
-            />
-          </div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="email"
+            placeholder="Please insert your email"
+            // required
+            className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm bg-gray-50 placeholder:text-gray-400 focus:border-0 sm:text-sm sm:leading-6"
+            // className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-blue-700 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <div className="my-4">
           <label
             htmlFor="password"
-            className="block text-sm font-medium leading-6 text-gray-800"
+            className="block text-sm font-medium leading-6 text-gray-800 mb-1"
           >
             Password
           </label>
-          <div className="mt-1">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              autoComplete="password"
-              required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-indigo-700 sm:text-sm sm:leading-6"
-            />
-          </div>
+
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="password"
+            placeholder="Please insert your password"
+            // required
+            className="block w-full rounded-md border-0 py-1.5 text-gray-800 shadow-sm bg-gray-50 placeholder:text-gray-400 focus:border-0 sm:text-sm sm:leading-6"
+          />
         </div>
         <button
           onClick={submit}
           type="submit"
-          className="text-white bg-indigo-950 w-full p-1.5 rounded-md"
+          className="text-white w-full p-1.5 rounded-md bg-blue-950 hover:bg-blue-900"
         >
-          {isFetching ? "...loading" : "Login to account"}
+          {isFetching ? "...loading" : "Create an account"}
         </button>
+        <div className="my-6 flex justify-between items-center w-full gap-2 md:w-3/4 m-auto flex-col md:flex-row">
+          <div className="flex items-center justify-center text-sm gap-2 cursor-pointer border rounded-full p-2 hover:bg-blue-900 hover:text-white w-full">
+            <FaFacebook size={25} />
+            <p>Proceed with Facebook</p>
+          </div>
+          <div className="flex items-center justify-center text-sm gap-2 cursor-pointer border rounded-full p-2  hover:bg-blue-900 hover:text-white w-full">
+            <FaGoogle size={25} />
+            <p>Proceed with Google</p>
+          </div>
+        </div>
+        <Link to={"/sign-up"} className="text-center mt-3 block text-sm ">
+          Don't yet have an account ?
+          <span className="text-blue-900"> Sign Up</span>
+        </Link>
       </form>
-      <Link to={"/sign-up"}>Sign up</Link>
-      <div className="flex justify-center"></div>
     </div>
   );
 };
